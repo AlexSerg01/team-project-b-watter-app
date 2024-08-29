f1c8ca345a969ecf07bdf79e60236ff9d7da62d0
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-29 15:17:01 +0300

    add error message

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export function env(name, defaultName) {
-    const value = process.env[name];
-    if(value) return value;
-    if(defaultName) return defaultName;
-    throw new Error('Oops');
+  const value = process.env[name];
+  if (value) return value;
+  if (defaultName) return defaultName;
+  throw new Error(`Missing: process.env['${name}'].`);
 }
