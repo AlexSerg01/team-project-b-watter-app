c536abbf92d913031b0a1c6ded1f9a1345801323
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-29 16:57:20 +0300

    add addWaterConsumption controller

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -1,14 +1,30 @@
-import waterService from '../services/water.js';
+import * as WaterService from '../services/water.js';
 
 export const getWaterConsumptionByMonth = async (req, res) => {
   const { month, year } = req.params;
   const userId = req.user._id;
 
-  const result = await waterService.getWaterConsumption(userId, month, year);
+  const result = await WaterService.getWaterConsumption(userId, month, year);
 
   res.status(200).json({
     status: 200,
-    message: `Info about water consumption per day for month period for userId: ${userId}`,
+    message: `Month period records of water consumption per day for userId: ${userId}`,
     data: result,
+  });
+};
+
+export const addWaterConsumption = async (req, res) => {
+  const waterConsumption = {
+    userId: req.user._id,
+    date: new Date().toISOString(),
+    amount: req.body.amount,
+  };
+
+  const newRecord = await WaterService.addWaterConsumption(waterConsumption);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully add new water consumption record',
+    data: newRecord,
   });
 };
