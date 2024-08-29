63e1914b600e12f9b28e7a9229ce7454b6dd3d0a
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-29 16:56:55 +0300

    add timestamps & versionKey

--- src/bd/models/water.js
+++ src/bd/models/water.js
@@ -1,19 +1,25 @@
 import mongoose from 'mongoose';
 
-const waterConsumptionSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+const waterConsumptionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
   },
-  date: {
-    type: Date,
-    required: true,
+  {
+    timestamps: true,
+    versionKey: false,
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-});
+);
 
 export default mongoose.model('WaterConsumption', waterConsumptionSchema);
