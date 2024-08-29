50242e8199c88c0772b832d185d94028056af4d0
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-29 15:17:38 +0300

    add get router

--- src/routers/water.js
+++ src/routers/water.js
@@ -1,7 +1,14 @@
-import { Router } from "express";
-import ctrlWrapper from "../utils/ctrlWrapper.js";
+import { Router } from 'express';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
+import { getWaterConsumptionByMonth } from '../controllers/water.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
+router.get(
+  '/:month/:year',
+  authenticate,
+  ctrlWrapper(getWaterConsumptionByMonth),
+);
 
 export default router;
