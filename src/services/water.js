4b3c9f27f35d043c842b93f3636c0820f1275510
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-29 19:41:13 +0300

    fix dailyNorm data

--- src/services/water.js
+++ src/services/water.js
@@ -1,17 +1,17 @@
 import WaterConsumptionCollection from '../bd/models/water.js';
 
-export const getWaterConsumption = async (userId, month, year) => {
+export const getWaterConsumption = async (userId, dailyNorm, month, year) => {
   const startDate = new Date(year, month - 1, 1);
   const endDate = new Date(year, month, 0);
 
-  const getwaterData = await WaterConsumptionCollection.find({
+  const getWaterData = await WaterConsumptionCollection.find({
     userId,
     date: { $gte: startDate, $lte: endDate },
   });
 
   const dailyData = {};
 
-  getwaterData.forEach((record) => {
+  getWaterData.forEach((record) => {
     const day = record.date.getDate();
     const monthName = new Intl.DateTimeFormat('en-US', {
       month: 'long',
@@ -24,8 +24,6 @@ export const getWaterConsumption = async (userId, month, year) => {
     dailyData[dayKey].totalAmount += record.amount;
     dailyData[dayKey].entries += 1;
   });
-
-  const dailyNorm = 1800; //CHANGE WHEN WATER RATE ENDPOINT WILL BE READY
 
   return Object.keys(dailyData).map((day) => ({
     date: day,
@@ -42,3 +40,5 @@ export const getWaterConsumption = async (userId, month, year) => {
 export const addWaterConsumption = async (record) => {
   return WaterConsumptionCollection.create(record);
 };
+
+// -------------------------------------------------------
