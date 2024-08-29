1bd6a23204715a1ccdeb91172fadb58b3e7e5ca0
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-29 19:41:22 +0300

    add 404 error check

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -1,10 +1,22 @@
+import createHttpError from 'http-errors';
+
 import * as WaterService from '../services/water.js';
 
-export const getWaterConsumptionByMonth = async (req, res) => {
+export const getWaterConsumptionByMonth = async (req, res, next) => {
   const { month, year } = req.params;
   const userId = req.user._id;
+  const dailyNorm = req.user.dailyWaterIntake;
 
-  const result = await WaterService.getWaterConsumption(userId, month, year);
+  const result = await WaterService.getWaterConsumption(
+    userId,
+    dailyNorm,
+    month,
+    year,
+  );
+
+  if (result === null || result.userId.toString() !== req.user._id.toString()) {
+    return next(createHttpError(404, 'Records not found'));
+  }
 
   res.status(200).json({
     status: 200,
@@ -13,18 +25,22 @@ export const getWaterConsumptionByMonth = async (req, res) => {
   });
 };
 
+// -------------------------------------------------------
+
 export const addWaterConsumption = async (req, res) => {
   const waterConsumption = {
     userId: req.user._id,
     date: new Date().toISOString(),
-    amount: req.body.amount,
+    amount: parseInt(req.body.amount),
   };
 
   const newRecord = await WaterService.addWaterConsumption(waterConsumption);
 
   res.status(201).json({
     status: 201,
-    message: 'Successfully add new water consumption record',
+    message: 'New water consumption record has been added successfully!',
     data: newRecord,
   });
 };
+
+// -------------------------------------------------------
