a8786359ad7dae73907d516c4c812b728aaa284b
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-08-31 18:43:06 +0300

    auth docs - done

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,11 +4,12 @@ import ctrlWrapper from "../utils/ctrlWrapper.js";
 import { registrationValidationSchema, loginSchema } from "../validation/auth.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { registerController, loginController, logoutController } from "../controllers/auth.js";
+import {authenticate} from '../middlewares/authenticate.js'
 
 const router = Router();
 
 router.post('/register', validateBody(registrationValidationSchema), ctrlWrapper(registerController));
 router.post('/login', validateBody(loginSchema), ctrlWrapper(loginController));
-router.post('/logout', ctrlWrapper(logoutController));
+router.post('/logout', authenticate, ctrlWrapper(logoutController));
 
 export default router;
