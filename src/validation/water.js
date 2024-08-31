2c9b1dc42ef39d5f5eb1e2538cd1f8be96ef8623
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-31 20:11:18 +0300

    add update validation

--- src/validation/water.js
+++ src/validation/water.js
@@ -3,3 +3,7 @@ import Joi from 'joi';
 export const addAmountOfConsumedWaterSchema = Joi.object({
   amount: Joi.number().integer().positive().max(5000).required(),
 });
+
+export const updateAmountOfConsumedWaterSchema = Joi.object({
+  amount: Joi.number().integer().positive().max(5000),
+});
