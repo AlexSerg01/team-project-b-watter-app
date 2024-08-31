391dd6e7e0c381125d9ecaca13a3e97ae1b627ad
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-31 20:11:43 +0300

    add update router

--- src/routers/water.js
+++ src/routers/water.js
@@ -3,27 +3,35 @@ import { Router } from 'express';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import { authenticate } from '../middlewares/authenticate.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { addAmountOfConsumedWaterSchema } from '../validation/water.js';
+import {
+  addAmountOfConsumedWaterSchema,
+  updateAmountOfConsumedWaterSchema,
+} from '../validation/water.js';
 import {
   getWaterConsumptionByMonth,
-  addWaterConsumption, getDailyWaterConsumptionController
+  addWaterConsumption,
+  updateWaterConsumption,
+  getDailyWaterConsumptionController,
 } from '../controllers/water.js';
 
 const router = Router();
 
-router.get(
-  '/:month/:year',
-  authenticate,
-  ctrlWrapper(getWaterConsumptionByMonth),
-);
+router.use('/', authenticate);
 
 router.post(
   '/',
-  authenticate,
   validateBody(addAmountOfConsumedWaterSchema),
   ctrlWrapper(addWaterConsumption),
 );
 
-router.get('/water-consumption', authenticate, ctrlWrapper(getDailyWaterConsumptionController))
+router.patch(
+  '/:id',
+  validateBody(updateAmountOfConsumedWaterSchema),
+  ctrlWrapper(updateWaterConsumption),
+);
+
+router.get('/', ctrlWrapper(getDailyWaterConsumptionController));
+
+router.get('/:month/:year', ctrlWrapper(getWaterConsumptionByMonth));
 
 export default router;
