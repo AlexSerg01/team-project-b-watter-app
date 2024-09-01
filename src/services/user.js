baf3e2eb40aa672ca4cbf5edcf3c2a62982e8b29
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-09-01 18:54:44 +0300

    update daily water intake service

--- src/services/user.js
+++ src/services/user.js
@@ -29,7 +29,7 @@ export const patchUserDailyWaterIntake = async (id, data, options = {}) => {
         ...options,
     });
 
-    return updatedUser.dailyWaterIntake
+    return updatedUser.value.dailyWaterIntake
 };
 
 
