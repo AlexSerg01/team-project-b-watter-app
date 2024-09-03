b5782805bbc15ece2b2fd3eb3b354bd460f2441d
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-09-03 12:46:57 +0200

    Add_reset_User_password

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
-import path from 'path'
+import path from 'path';
 
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
+
+export const TEMPLATE_DIR = path.resolve('src', 'templates');
--- src/services/auth.js
+++ src/services/auth.js
@@ -2,39 +2,42 @@ import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { randomBytes } from 'crypto';
 import jwt from 'jsonwebtoken';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
 import { usersCollection } from '../bd/models/user.js';
 import { sessionCollection } from '../bd/models/session.js';
 import { FIFTEEN_MINUTES, SMTP } from '../constants/authConstants.js';
 import { env } from '../utils/env.js';
 import { sendEmail } from '../utils/sendMail.js';
+import { TEMPLATE_DIR } from '../constants/index.js';
 
 export const registerUser = async (data) => {
-    const user = await usersCollection.findOne({email: data.email});
-    if(user) throw createHttpError(409, 'Email in use');
+  const user = await usersCollection.findOne({ email: data.email });
+  if (user) throw createHttpError(409, 'Email in use');
 
-    const encryptedPassword = await bcrypt.hash(data.password, 10);
-    return await usersCollection.create({...data, password: encryptedPassword});
-
+  const encryptedPassword = await bcrypt.hash(data.password, 10);
+  return await usersCollection.create({ ...data, password: encryptedPassword });
 };
 
 export const loginUser = async (data) => {
-    const user = await usersCollection.findOne({email: data.email});
-    if(!user) throw createHttpError(404, 'User not found');
-    const ifPasswordsEqual = await bcrypt.compare(data.password, user.password);
-    if(!ifPasswordsEqual) throw createHttpError(401, 'Unauthorized');
+  const user = await usersCollection.findOne({ email: data.email });
+  if (!user) throw createHttpError(404, 'User not found');
+  const ifPasswordsEqual = await bcrypt.compare(data.password, user.password);
+  if (!ifPasswordsEqual) throw createHttpError(401, 'Unauthorized');
 
-    await sessionCollection.deleteOne({userId: user._id});
-    const accessToken = randomBytes(30).toString('base64');
+  await sessionCollection.deleteOne({ userId: user._id });
+  const accessToken = randomBytes(30).toString('base64');
   return await sessionCollection.create({
     userId: user._id,
     accessToken,
     accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
   });
-
 };
 
-export const logoutUser = (sessionId) => sessionCollection.deleteOne({ _id: sessionId });
+export const logoutUser = (sessionId) =>
+  sessionCollection.deleteOne({ _id: sessionId });
 
 export const requestResetToken = async (email) => {
   const user = await usersCollection.findOne({ email });
@@ -51,11 +54,51 @@ export const requestResetToken = async (email) => {
       expiresIn: '15m',
     },
   );
+  const templateFile = path.join(TEMPLATE_DIR, 'reset-password-email.html');
+
+  const templateSource = await fs.readFile(templateFile, { encoding: 'utf-8' });
+
+  const template = handlebars.compile(templateSource);
+
+  const html = template({
+    name: user.name,
+    link: `https://team-project-watter-app.vercel.app/reset-password/${resetToken}`,
+  });
 
   await sendEmail({
     from: env(SMTP.SMTP_FROM),
     to: email,
     subject: 'Reset your password',
-    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+    html,
   });
+};
+
+export const resetPassword = async (password, token) => {
+  try {
+    const decoded = jwt.verify(token, env('JWT_SECRET'));
+
+    console.log(decoded);
+
+    const user = await usersCollection.findOne({
+      _id: decoded.sub,
+      email: decoded.email,
+    });
+
+    if (user === null) {
+      throw createHttpError(404, 'User not found');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await usersCollection.findByIdAndUpdate(user._id, {
+      password: hashedPassword,
+    });
+  } catch (error) {
+    if (
+      error.name === 'TokenExpiredError' ||
+      error.name === 'JsonWebTokenError'
+    ) {
+      throw createHttpError(401, 'Token not valid');
+    }
+    throw error;
+  }
 };
