f9cb9fa3a67029e47b2ebf3a004467dc20983e74
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-09 00:18:01 +0300

    remove validation

--- src/validation/water.js
+++ src/validation/water.js
@@ -2,10 +2,10 @@ import Joi from 'joi';
 
 export const addAmountOfConsumedWaterSchema = Joi.object({
   amount: Joi.number().integer().positive().max(5000).required(),
-  time: Joi.string().pattern(/^([01]\d|2[0-3]):([0-5]\d)$/),
+  time: Joi.string(),
 });
 
 export const updateAmountOfConsumedWaterSchema = Joi.object({
   amount: Joi.number().integer().positive().max(5000).required(),
-  time: Joi.string().pattern(/^([01]\d|2[0-3]):([0-5]\d)$/),
+  time: Joi.string(),
 });
